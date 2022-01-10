e00fbbe2a148c4c8944cd3f1f40ade1e6572b9d3
hamkis87 <hamidkisha@gmail.com>
2022-01-10 01:22:16 +0100

    implementing read graph

--- 01_GraphViz/src/main.cpp
+++ 01_GraphViz/src/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "SimpleGraph.h"
 
 using std::cout;	using std::endl;
+using std::string;  using std::cin;
+using std::ifstream;
 
 void Welcome();
+void readGraph(SimpleGraph & graph);
+string GetLine();
 
 // Main method
 int main() {
     Welcome();
+    SimpleGraph myGraph;
+    readGraph(myGraph);
     return 0;
 }
 
@@ -18,4 +26,24 @@ void Welcome() {
     cout << "This program uses a force-directed graph layout algorithm" << endl;
     cout << "to render sleek, snazzy pictures of various graphs." << endl;
     cout << endl;
+}
+
+void readGraph(SimpleGraph & graph) {
+    ifstream graphFile;
+    cout << "Enter the name of graph file: ";
+    string graphFileName = GetLine();
+    graphFile.open(graphFileName);
+    if (!graphFile.is_open()) {
+        std::cerr << "Cannot find the file" << graphFileName << endl;
+    }
+    else {
+
+
+    }
+}
+
+string GetLine() {
+    string result;
+    getline(cin, result);
+    return result;
 }
