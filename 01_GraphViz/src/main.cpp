4bfbac9b46b58be0ade99cc1a8ab60eb5ff078e4
hamkis87 <hamidkisha@gmail.com>
2022-03-14 00:52:48 +0100

    completing read graph functionality

--- 01_GraphViz/src/main.cpp
+++ 01_GraphViz/src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include "SimpleGraph.h"
 
-using std::cout;	using std::endl;
-using std::string;  using std::cin;
-using std::ifstream;
+using namespace std;
 
 void Welcome();
 void readGraph(SimpleGraph & graph);
+bool wellFormedNodesNumber(ifstream & graphFile, SimpleGraph & graph);
+bool wellFormedEdges(ifstream & graphFile, SimpleGraph & graph);
 string GetLine();
 
 // Main method
@@ -29,17 +30,81 @@ void Welcome() {
 }
 
 void readGraph(SimpleGraph & graph) {
+    int numberOfNodes;
     ifstream graphFile;
     cout << "Enter the name of graph file: ";
     string graphFileName = GetLine();
     graphFile.open(graphFileName);
     if (!graphFile.is_open()) {
-        std::cerr << "Cannot find the file" << graphFileName << endl;
+        cerr << "Cannot find the file" << graphFileName << endl;
     }
     else {
-
+        cout << "file read success" << endl;
+        if(wellFormedNodesNumber(graphFile, graph)) {
+            cout << "number of nodes = " << graph.nodes.size() << endl;
+            if(wellFormedEdges(graphFile, graph)) {
+                cout << "well formed edges" << endl;
+                cout << "No. of Edges: " << graph.edges.size() << endl;
+            }
+        }
+        else {
+            cerr << "Number of Nodes is not well-formed." << endl;
+        }
 
     }
+}
+
+bool wellFormedNodesNumber(ifstream & graphFile, SimpleGraph & graph) {
+    string line;
+    bool result = false;
+    if(getline(graphFile, line)) {
+        stringstream converter;
+        converter << line;
+        /* Try reading an int, continue if we succeeded. */
+        int inputInt;
+        if(converter >> inputInt) {
+            char remaining;
+            if(converter >> remaining) // Something's left, input is invalid
+                cout << "Unexpected character: " << remaining << endl;
+            else {
+                graph.nodes.resize(inputInt);
+                result = true;
+            }
+
+        }
+    }
+    return result;
+}
+
+bool wellFormedEdges(ifstream & graphFile, SimpleGraph & graph) {
+    string line;
+    bool result = false;
+    while(getline(graphFile, line)) {
+        stringstream converter;
+        converter << line;
+        /* Try reading an int, continue if we succeeded. */
+        int start, end;
+        if(converter >> start && converter >> end) {
+            char remaining;
+            if(converter >> remaining) {// Something's left, input is invalid
+                cout << "Unexpected character: " << remaining << endl;
+                return false;
+            }
+            else {
+                cout << "start: " << start << "end: " << end << endl;
+                Edge edge;
+                edge.start = start;
+                edge.end = end;
+                graph.edges.push_back(edge);
+                result = true;
+            }
+
+        }
+        else {
+            return false;
+        }
+    }
+    return result;
 }
 
 string GetLine() {
